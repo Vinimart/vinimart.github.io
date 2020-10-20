96efb9768e5156e04df592cf49f38ef843d10415
Vinicius Martins <vinimartdev@gmail.com>
2020-10-20 18:59:32 -0300

    Portfolio Modal Component ID increment when rendered

--- js/github.js
+++ js/github.js
@@ -1,27 +1,40 @@
+// This class is responsable for fetching the data from Github API.
 class GetApi {
 	constructor() {
-		this.view = new GithubApiView();
+        this.view = new GithubApiView();
+        // This number is a reference to portfolio modal's ID.
+		this.modalNumber = 1;
 	}
 
-	async githubFetch(repo) {
-        const response = await fetch(`https://api.github.com/repos/Vinimart/${repo}`);
-        
-		if (response.status === 200) {
-			const data = await response.json();
-			this.setResults(data);
+	// This method is responsable for validating the repository name and setting the argument for githubFetch().
+	getInfo(repo) {
+		if (repo) {
+			// Increments modal number every time when a portfolio component is rendered;
+			return this.githubFetch(repo, this.modalNumber++);
 		} else {
-			console.error(`Could not find "${repo}" repository. Request status: ${response.status}`);
+			return console.error("Please insert a valid repository name as argument for getInfo method.");
 		}
 	}
 
-	setResults(data) {
+	// Request
+	async githubFetch(repo, modal) {
+		const response = await fetch(`https://api.github.com/repos/Vinimart/${repo}`);
+
+		if (response.status === 200) {
+			const data = await response.json();
+			this.setResults(data, modal);
+		} else console.error(`Could not find "${repo}" repository. Request status: ${response.status}`);
+	}
+
+	// Set the response from the request to the View layer.
+	setResults(data, modal) {
 		this.data = data;
+		this.modalNumber = modal;
 		this.url = this.data.svn_url;
 		this.name = this.data.name;
 		this.desc = this.data.description;
 		this.info = [this.url, this.name, this.desc];
-
-		this.view.setInfo(this.info);
+		this.view.setInfo(this.info, this.modalNumber);
 	}
 }
 
@@ -30,26 +43,24 @@ class GithubApiView {
 		this.portfolioContainer = document.getElementById("portfolioContainer");
 	}
 
-	getInfo(repo) {
-		this.getApi = new GetApi();
-
-		if (repo) {
-			return this.getApi.githubFetch(repo);
-		} else {
-			return console.error("Please insert a valid repository name as argument for getInfo method.");
-		}
-	}
-
-	setInfo(info) {
+	// Set info arguments into the renderer method.
+	setInfo(info, modal) {
 		this.info = info;
-		this.renderPortfolioGrid(this.info[1], this.info[0]);
+		this.portfolioModal = modal;
+		this.renderPortfolioGrid(this.info[1], this.info[0], this.portfolioModal);
 	}
 
-	portfolioTemplate(name, url) {
+	// Insert portfolio component after the last child of main container.
+	renderPortfolioGrid(name, url, modal) {
+		this.portfolioContainer.insertAdjacentHTML("beforeend", this.portfolioTemplate(name, url, modal));
+	}
+
+	// Portfolio item component template.
+	portfolioTemplate(name, url, modal) {
 		return `
         <div class="col-lg-4 col-sm-6 mb-4">
             <div class="portfolio-item">
-                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${modal}">
                     <div class="portfolio-hover">
                         <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                     </div>
@@ -76,9 +87,5 @@ class GithubApiView {
             </div>
         </div>
         `;
-	}
-
-	renderPortfolioGrid(name, url) {
-		this.portfolioContainer.insertAdjacentHTML("beforeend", this.portfolioTemplate(name, url));
 	}
 }
