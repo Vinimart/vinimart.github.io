bbffb4520730c0761b0a458c14ffa473093ce5e0
Vinicius Martins <vinimartdev@gmail.com>
2020-10-24 19:33:37 -0300

    Preloader

---
+++ js/preloader.js
@@ -0,0 +1,5 @@
+// Toggle the visibility of the preloader animation on page load.
+window.addEventListener("load", function () {
+	const preloader = document.getElementById("preloader");
+	preloader.classList.toggle("hidden");
+});
