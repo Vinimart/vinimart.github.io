37da333d20419b5c2c28e6f0366e059487365fee
Vinicius Martins <vinimartdev@gmail.com>
2020-10-30 14:06:13 -0300

    Test: portfolioModal

--- tests/portfolioModal.js
+++ tests/portfolioModal.js
@@ -1,17 +1,17 @@
 // End to End Tests with Cypress
-
+// Portfolio Modal Component
 describe("Check Portfolio Modal", () => {
 	it("Visit", () => {
 		cy.visit("/");
 	});
 
 	it("Check visibility", () => {
-        cy.get("#portfolioModal1 > .modal-dialog > .modal-content")
+        cy.get("#portfolioModal1")
             .should("not.be.visible");
 	});
 
 	it("Check click", () => {
-        cy.get(":nth-child(1) > .portfolio-item > .portfolio-link > .portfolio-hover")
+        cy.get(":nth-child(1) > .portfolio-item > .portfolio-link")
             .click();
         cy.get("#portfolioModal1")
             .should("be.visible");
